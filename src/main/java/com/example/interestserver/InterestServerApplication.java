aaed322f53929d0541ca8d6eee82f72953fd7f21
김주환 <juhwankim@gimjuhwan-ui-MacBookAir.local>
2023-10-14 15:27:24 +0900

    Intial Commit

---
+++ src/main/java/com/example/interestserver/InterestServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.interestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterestServerApplication.class, args);
+    }
+
+}
