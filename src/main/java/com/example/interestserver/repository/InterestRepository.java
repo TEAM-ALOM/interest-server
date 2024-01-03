4386d4c0d9693af08a19ae6b40f4d4f88444542b
김주환 <juhwankim@gimjuhwans-MacBook-Air.local>
2024-01-04 04:21:41 +0900

    complete all

--- src/main/java/com/example/interestserver/repository/InterestRepository.java
+++ src/main/java/com/example/interestserver/repository/InterestRepository.java
@@ -1,4 +1,20 @@
 package com.example.interestserver.repository;
 
-public interface InterestRepository {
+import com.example.interestserver.domain.Interest;
+import com.example.interestserver.domain.Field;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InterestRepository extends JpaRepository<Interest, Long> {
+
+    Interest findByMemberId(String memberId);
+
+    List<Interest> findByLatitudeBetweenAndLongitudeBetween(double minLat, double maxLat, double minLong, double maxLong);
+
+    List<Interest> findByScoreGreaterThan(double score);
+
+    List<Interest> findByField(Field field);
 }
