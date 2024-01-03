0cc47dc50c56e02f4340077de5e48544abeb9177
김주환 <juhwankim@gimjuhwans-MacBook-Air.local>
2024-01-04 01:54:25 +0900

    Initial Commit

---
+++ src/test/java/com/example/interestserver/InterestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterestServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
